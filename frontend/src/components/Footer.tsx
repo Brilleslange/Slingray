546852dbaf2f781666d68ac064f049b69d455866
Brilleslange <ingeborg.langli@gmail.com>
2025-08-29 11:02:50 +0200

    Adds Ko-fi button to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,9 +3,10 @@ import GitHub from '../assets/github-mark.svg?react';
 
 export default function Footer() {
     return (
-        <footer className={"footer footer-horizontal p-4 justify-end"}>
+        <footer className={"footer footer-horizontal p-4 justify-end align-center"}>
             <a href={"mailto:slingray@brilleslange.net"}><Envelope className={"size-6"} /></a>
             <a href={"https://github.com/brilleslange/slingray"} ><GitHub className={"size-6"} /></a>
+            <a href={"https://ko-fi.com/brilleslange"}><img src={"/src/assets/ko-fi.png"} alt={"Ko-Fi"} className={"h-6"}/></a>
         </footer>
     )
 }
