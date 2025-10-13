1baed9cc7c5c423e3c1283240c096886acba4ebb
Brilleslange <ingeborg.langli@gmail.com>
2025-10-13 16:51:10 +0200

    Removes question from Help section.

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -84,16 +84,6 @@ export default function Help() {
                         "The higher the score, the more likely the color."
                     }</div>
                 </div>
-                <div className={"collapse bg-base-200"}>
-                    <input type={"checkbox"}/>
-                    <div className={"collapse-title"}>
-                        Why "Slingray"?
-                    </div>
-                    <div className={"collapse-content text-sm"}>{
-                        "My Twilight Imperium group shortens the Sling Relay technology to Slingray, " +
-                        "so it's an homage to the people I play the game with."
-                    }</div>
-                </div>
             </div>
         </div>
     </div>)
